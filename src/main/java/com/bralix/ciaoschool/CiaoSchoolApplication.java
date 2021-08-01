7d7fba39ef308910096bc650a140a2f9aeae41e3
bralix <bralix@mail.ru>
2021-08-01 16:13:55 +0200

    Initial Commit

---
+++ src/main/java/com/bralix/ciaoschool/CiaoSchoolApplication.java
@@ -0,0 +1,13 @@
+package com.bralix.ciaoschool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CiaoSchoolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CiaoSchoolApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bralix/ciaoschool/CiaoSchoolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bralix.ciaoschool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CiaoSchoolApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
